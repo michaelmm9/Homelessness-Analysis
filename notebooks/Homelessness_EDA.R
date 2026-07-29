a3e8c6da789e9b5286072b7be4bb96133b7ba11e
Michael Medina <michaelmm9@gmail.com>
2026-07-29 15:18:20 -0700

    removed census key from R script

--- notebooks/Homelessness_EDA.R
+++ notebooks/Homelessness_EDA.R
@@ -3,7 +3,6 @@
 library(tidycensus)
 library(tidyverse)
 
-census_api_key("bc82d02d37a5034046c8d47f56e5cbf5998a4c19", install = T)
 
 Sys.getenv("CENSUS_API_KEY")
 
@@ -24,3 +23,5 @@ head(acs)
 acs_wide <- acs %>%
   select(-moe) %>%
   pivot_wider(., names_from = variable, values_from = estimate)
+
+hist(acs_wide$median_income, main = "Histogram of Median Income", xlab = "Median Income", col = "lightblue", border = "black")
